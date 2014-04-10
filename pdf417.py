499eb954202bf228c9bde5bf6bbc44b13a151b56
Mariano Cabezas <mcabezas@gaa9.com>
2014-04-10 17:48:05 +0200

    pep8

--- pdf417.py
+++ pdf417.py
@@ -12,6 +12,7 @@ pdf_mode = 'text'
 text_submode = 'upper'
 text_shift = False
 
+
 def add_quiet_zone(im):
     box = (15, 15, im.size[0]+15, im.size[1]+15)
     img = Image.new('L', (im.size[0]+30, im.size[1]+30), 'white')
@@ -21,6 +22,7 @@ def add_quiet_zone(im):
 
 def get_img(img_path):
     return Image.open(img_path)
+
 
 def print_img_info(im):
     info = im.info
@@ -32,14 +34,16 @@ def print_img_info(im):
     print 'image band: ', im.getbands()
     print 'image box: ', im.getbbox()
 
+
 def each_row(im, start, end, step=1):
     for y in range(start, end, step):
         yield list(im.crop((2, y, im.size[0]-2, y+1)).getdata())
 
-# not in use
+
 def each_column(im, start, end, step=1):
     for x in range(start, end, step):
         yield list(im.crop((x, 0, x+1, im.size[1])).getdata())
+
 
 def reformat(row_data):
     return [(i[0], len(list(i[1]))) for i in itertools.groupby(row_data)]
@@ -48,14 +52,18 @@ def reformat(row_data):
 def get_min_width(row):
     return row[1][1]
 
+
 def row2syms(row, mw):
     return "".join([str(i[1]/mw) for i in row])
 
+
 def get_cluster(sym):
-    return (int(sym[0])-int(sym[2])+int(sym[4])-int(sym[6])+9)%9
+    return (int(sym[0])-int(sym[2])+int(sym[4])-int(sym[6])+9) % 9
 
 
 pdf417_flag = False
+
+
 def get_codeword(syms, which):
     global pdf417_flag
     start = which*8-8
@@ -72,11 +80,12 @@ def get_codeword(syms, which):
 
     cluster = get_cluster(sym)
     k = cluster/3
-    for j in range(0,929):
+    for j in range(0, 929):
         if sym == codewords_tbl[k][j]:
-            return (k, j)
+            return k, j
 
     return False
+
 
 def decode_part(part):
     global text_submode, text_shift
@@ -184,10 +193,11 @@ def decode_cw(cw):
 
 
 def decode_text(cw):
-    H = cw / 30
-    L = cw % 30
 
-    return decode_part(H), decode_part(L)
+    h = cw / 30
+    l = cw % 30
+
+    return decode_part(h), decode_part(l)
 
 
 def decode_number(cw):
@@ -198,39 +208,53 @@ def decode_number(cw):
 
 def decode_byte(cw):
     dict_eascii = {
+        128: 'Ç',
+        129: 'ü',
+        130: 'é',
         168: '¿'
     }
     return dict_eascii[cw], ''
 
 
 def get_cwinfo(codewords):
-    l1 = codewords[0][0]
+    #l1 = codewords[0][0]
     l2 = codewords[1][0]
     l3 = codewords[2][0]
     length = len(codewords)
-    z = l2%30
-    v = l3%30
-    error_level = (z - (length-1)%3)/3
+    z = l2 % 30
+    v = l3 % 30
+    error_level = (z - (length-1) % 3)/3
     num_of_rows = v+1
-    return dict({'error_level':error_level, 'num_of_rows':num_of_rows})
+    return dict({
+        'error_level': error_level,
+        'num_of_rows': num_of_rows
+    })
+
 
 def filter_quitezone(row_data):
-    if row_data[0][0] == 255:  row_data = row_data[1:]
-    if row_data[-1][0] == 255: row_data = row_data[0:-1]
+    if row_data[0][0] == 255:
+        row_data = row_data[1:]
+    if row_data[-1][0] == 255:
+        row_data = row_data[0:-1]
     return row_data
+
 
 def filter_se_pattern(cw_of_row):
     return [i[1] for i in cw_of_row[1:-1]]
 
+
 def filter_err(codewords_list, error_level):
     return codewords_list[:-2**(error_level+1)]
+
 
 def filter_row_indicator(codewords):
     return [k for i in codewords for k in i[1:-1]]
 
+
 def get_content(codewords):
     skip = ['ll', 'ps', 'ml', 'al', 'pl', 'as']
-    return "".join( [str(k) for i in codewords for k in i if k not in skip] )
+    return "".join([str(k) for i in codewords for k in i if k not in skip])
+
 
 def _reset_global_vars():
     global text_shift, text_submode, pdf417_flag
@@ -238,9 +262,9 @@ def _reset_global_vars():
     text_submode = "upper"
     pdf417_flag = False
 
-        
+
 def pdf417_decode(img_path):
-    mw = 0 # min-width of the barcode
+    mw = 0  # min-width of the barcode
     codewords = []
     im = add_quiet_zone(get_img(img_path))
     im_h = im.size[1]
@@ -269,7 +293,7 @@ def pdf417_decode(img_path):
         if cw_of_row not in codewords:
             codewords.append(cw_of_row)
 
-    info =  get_cwinfo(codewords)
+    info = get_cwinfo(codewords)
     codewords_list = [cw for row in codewords for cw in row[1:-1]]
     codewords_noerr = filter_err(codewords_list, info['error_level'])
 
